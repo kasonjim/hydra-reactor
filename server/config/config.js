480fec3ba9b45807038e1495e2271065a21146f6
Danya D. Smith <danyadsmith@gmail.com>
2017-04-01 04:58:11 -0400

    Adds Additional Configuration Variables to config.js
    Now, database connections will expect environment variables to be
    present. Configure the following:
    
    process.env.MONGODB_URI_HYDRA
    process.env.MONGODB_HYDRA_USER
    process.env.MONGODB_HYDRA_PWD

---
+++ server/config/config.js
@@ -0,0 +1,17 @@
+// GOOGLE PLACES API CONFIGURATION
+exports.gpPlaceTextSearchUrl = 'https://maps.googleapis.com/maps/api/place/textsearch';
+exports.gpPlacePhotoSearchURl = 'https://maps.googleapis.com/maps/api/place/photo';
+exports.gpApiKey = process.env.GOOGLE_PLACES_API_KEY;
+exports.gpOutputFormat = process.env.GOOGLE_PLACES_OUTPUT_FORMAT || 'json';
+exports.gpPhotoMaxWidth = '1080';
+
+// YELP API CONFIGURATION
+exports.yelpToken = process.env.YELP_TOKEN;
+
+// DATABASE CONFIGURATION
+exports.dbUri = process.env.MONGODB_URI_HYDRA;
+exports.dbUser = process.env.MONGODB_HYDRA_USER;
+exports.dbPwd = process.env.MONGODB_HYDRA_PWD;
+
+// SERVER
+exports.port = process.env.PORT || 3000;
