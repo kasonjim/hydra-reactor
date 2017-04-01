e08ec3c5f5613361ce8be5112b15dc81f331f53a
Danya D. Smith <danyadsmith@gmail.com>
2017-04-01 19:09:33 -0400

    Migrate UserSchema methods from Legacy Model to Redesigned Model
    * generateToken
    * removeToken
    * findByToken
    * findByCredentials
    * pre

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+mongoose.Promise = global.Promise;
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema();
@@ -36,6 +42,85 @@ UserSchema.add({
     type: Schema.Types.ObjectId,
     ref: 'Trip'
   }]
+});
+
+UserSchema.methods.generateToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toString(), access}, 'somesecret');
+
+  user.tokens.push({access, token});
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+// 'UserSchema.statics' is a collection of methods on the UserSchema model
+UserSchema.statics.findByToken = function (token) {
+  //var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'somesecret');
+  } catch (err) {
+    return Promise.reject(err);
+  }
+  // find user by id and token
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
+// find user by email and password
+UserSchema.statics.findByCredentials = function (email, password) {
+  //var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      console.log('Promise.reject');
+      return Promise.reject();
+    }
+    // because bcrypt didn't support Promises, use generic Promise
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject(err);
+        }
+      });
+    });
+  });
+};
+
+// Mongoose middleware, runs before each 'save'
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+  // hash user password using salt 10 times
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
 });
 
 var User = mongoose.model('User', UserSchema);
